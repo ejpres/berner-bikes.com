7d474dfe47c06bacb393876771b0dbe21849d080
Eric Nordmeyer <eric@nordmeyer.name>
2011-07-01 12:38:34 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Berner::Application.config.secret_token = 'f019888f49229c8a5e2348d1b083c858b540f0d21792eac03ae0ffc753958628cdc71e62a866a8c9a3ccc7c6e87baf069e8e86bf8cea1dcf83661561bd666ccc'
